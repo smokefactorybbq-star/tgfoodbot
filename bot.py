db202d5aa5a41f5056a6bd21d5809818d7762e96
smokefactorybbq-star <smokefactorybbq@gmail.com>
2026-08-08 09:18:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -116,7 +116,7 @@ LOYALTY_SETTLE_URL = f"{WEBAPP_URL}/api/loyalty/settle"
 
 PRINT_URL = os.getenv(
     "PRINT_URL",
-    "https://f20d-171-6-240-2.ngrok-free.app/order",
+    "https://b1d3-171-6-240-2.ngrok-free.app/order",
 )
 
 
